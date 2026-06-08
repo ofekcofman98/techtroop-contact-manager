3b95c17f1b881226c5d6839a07df4b67c21f85ec
ofek <ofekcofman98@gmail.com>
2026-06-08 19:25:37 +0300

    Built skeleton

---
+++ commands/commandHandler.js
@@ -0,0 +1,11 @@
+// const commands = {
+//     add: new AddCommand(),
+//     delete: new DeleteCommand(),
+//     search: new SearchCommand(),
+//     list: new ListCommand(),
+//     help: new HelpCommand()
+// };
+
+
+
+// commands[command].execute(args);
